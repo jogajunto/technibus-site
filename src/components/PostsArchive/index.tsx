88090a229e76622d8f191215c2ccce4c01b5ff7d
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-05 21:44:30 +0100

    refactor(components): update Card and PostsArchive to support Search payload types

--- src/components/PostsArchive/index.tsx
+++ src/components/PostsArchive/index.tsx
@@ -1,16 +1,16 @@
-import { Post } from "@/payload-types";
+import { Post, Search } from "@/payload-types";
 
-import { PostCard } from "@/components/PostCard";
+import { Card } from "../Card";
 
 type PostArchiveProps = {
-  posts: Post[];
+  posts: Post[] | Search[];
 };
 
 export function PostArchive({ posts }: PostArchiveProps) {
   return (
     <div className="container grid gap-10 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post, index) => (
-        <PostCard key={index} {...post} />
+        <Card key={index} {...post} />
       ))}
     </div>
   );
