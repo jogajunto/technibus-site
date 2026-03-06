c529ff7ddc7b6c2c2cbdea171395dbd4b14806c3
Matheus Carvalho <henriquematheus.c@gmail.com>
2026-03-06 07:45:25 -0300

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,38 @@
-import { fetchAllPosts } from "@/collections/Posts/data";
 import type { MetadataRoute } from "next";
+
+import { fetchAllCategories } from "@/collections/Categories/data";
+import { fetchAllPosts } from "@/collections/Posts/data";
+import { fetchAllUsers as fetchAllAuthors } from "@/collections/Users/data";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await fetchAllPosts();
+  const categories = await fetchAllCategories();
+  const authors = await fetchAllAuthors();
 
-  // Páginas estáticas
   const sitemap: MetadataRoute.Sitemap = [
     {
       url: `${process.env.SITE_URL}`,
     },
-    {
-      url: `${process.env.SITE_URL}/politica-de-privacidade`,
-    },
   ];
 
-  // Posts
-  posts.forEach((service) => {
+  posts.forEach((post) => {
     sitemap.push({
-      url: `${process.env.SITE_URL}/${service.slug}`,
-      lastModified: new Date(service.updatedAt),
+      url: `${process.env.SITE_URL}${post.relPermalink}`,
+      lastModified: new Date(post.updatedAt),
+    });
+  });
+
+  categories.forEach((category) => {
+    sitemap.push({
+      url: `${process.env.SITE_URL}${category.relPermalink}`,
+      lastModified: new Date(category.updatedAt),
+    });
+  });
+
+  authors.forEach((author) => {
+    sitemap.push({
+      url: `${process.env.SITE_URL}${author.relPermalink}`,
+      lastModified: new Date(author.updatedAt),
     });
   });
 
--- src/collections/Users/data.ts
+++ src/collections/Users/data.ts
@@ -7,6 +7,27 @@ import { User } from "@/payload-types";
 
 const payload = await getPayload({ config });
 
+export const fetchAllUsers = async (): Promise<User[]> => {
+  const { isEnabled: draft } = await draftMode();
+
+  const { docs: categories } = await payload.find({
+    collection: "users",
+    depth: 2,
+    draft,
+    limit: 0,
+    where: {
+      and: [...(draft ? [] : [{ _status: { equals: "published" } }])],
+    },
+  });
+
+  const filtered = categories.filter((user: User) => {
+    const posts = user?.posts?.docs ?? [];
+    return posts.length > 0;
+  });
+
+  return filtered;
+};
+
 export const fetchUserBySlug = async (slug: string): Promise<User> => {
   const { isEnabled: draft } = await draftMode();
   const data = await payload.find({
