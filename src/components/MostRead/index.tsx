4cdd53cc1f4a49fc31f8e169d57145ebb050233d
Matheus Carvalho <henriquematheus.c@gmail.com>
2026-03-09 03:58:14 -0300

    Update the list of most read posts

--- src/components/MostRead/index.tsx
+++ src/components/MostRead/index.tsx
@@ -1,9 +1,15 @@
-import { fetchLatestPosts } from "@/collections/Posts/data";
+import { fetchPostBySlug } from "@/collections/Posts/data";
 import { Card } from "../Card";
 import { SectionHeading, SectionHeadingTitle } from "../TitleWithDivider";
 
 export async function MostRead() {
-  const posts = await fetchLatestPosts({ limit: 5 });
+  const post01 = await fetchPostBySlug("o-historico-teste-do-primeiro-double-decker-brasileiro");
+  const post02 = await fetchPostBySlug("camara-aprova-regime-de-urgencia-para-votar-marco-legal-do-transporte-publico");
+  const post03 = await fetchPostBySlug("mercedes-benz-comercializa-50-chassis-of-1519-r-para-fretamento-rural");
+  const post04 = await fetchPostBySlug("operacao-da-antt-em-mg-retira-26-veiculos-clandestinos-de-circulacao");
+  const post05 = await fetchPostBySlug("consorcio-nova-via-mobilidade-e-habilitado-para-substituir-supervia-no-rio-de-janeiro");
+
+  const posts = [post01, post02, post03, post04, post05];
 
   return (
     <div className="space-y-8">
