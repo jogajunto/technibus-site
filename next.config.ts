0142a3298e0b6610924da5bac6a9d88c43765271
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-17 06:24:39 +0100

    remove RSCHeaders prevent cache by next

--- next.config.ts
+++ next.config.ts
@@ -1,69 +1,9 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
-import { Header } from "next/dist/lib/load-custom-routes";
 
 const ONE_DAY = 60 * 60 * 24;
 const ONE_WEEK = ONE_DAY * 7;
 const ONE_YEAR = ONE_DAY * 365;
-
-const RSCValidate = (): Promise<Header[]> | Header[] => {
-  const RSCCacheHeaders = [
-    {
-      key: "Cache-Control",
-      value: "private, no-cache, no-store, max-age=0, must-revalidate",
-    },
-    {
-      key: "Vary",
-      value: "RSC, Next-Router-State-Tree, Next-Router-Prefetch",
-    },
-  ];
-
-  return [
-    // ---------------------------------------------------------------
-    // REGRA 1: Protege quando tem a QUERY STRING (?_rsc=...)
-    // ---------------------------------------------------------------
-    {
-      source: "/:path*",
-      has: [
-        {
-          type: "query",
-          key: "_rsc",
-        },
-      ],
-      headers: RSCCacheHeaders,
-    },
-
-    // ---------------------------------------------------------------
-    // REGRA 2: Protege quando tem o HEADER 'RSC' (O lance do RSC: 1)
-    // (Essencial para redirects e requests onde a query string some)
-    // ---------------------------------------------------------------
-    {
-      source: "/:path*",
-      has: [
-        {
-          type: "header",
-          key: "RSC", // O Next.js verifica isso case-insensitive
-        },
-      ],
-      headers: RSCCacheHeaders,
-    },
-
-    // ---------------------------------------------------------------
-    // REGRA 3: Protege quando tem o HEADER de PREFETCH
-    // (Segurança extra para quando o link entra na tela)
-    // ---------------------------------------------------------------
-    {
-      source: "/:path*",
-      has: [
-        {
-          type: "header",
-          key: "Next-Router-Prefetch",
-        },
-      ],
-      headers: RSCCacheHeaders,
-    },
-  ];
-};
 
 const nextConfig: NextConfig = {
   turbopack: {},
@@ -88,7 +28,6 @@ const nextConfig: NextConfig = {
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
         ],
       },
-      ...(RSCValidate() as Header[]),
       {
         source: "/_next/static/:path*",
         headers: [{ key: "Cache-Control", value: `public, max-age=${ONE_YEAR}, s-maxage=${ONE_YEAR}, immutable` }],
