97eb747b0680d9b0885279b1416ee904c1738bc0
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-17 05:38:28 +0100

    fixed RSC Headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,73 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
+import { Header } from "next/dist/lib/load-custom-routes";
 
 const ONE_DAY = 60 * 60 * 24;
 const ONE_WEEK = ONE_DAY * 7;
 const ONE_YEAR = ONE_DAY * 365;
+
+const RSCValidate = (): Promise<Header[]> | Header[] => {
+  const RSCCacheHeaders = [
+    {
+      key: "Cache-Control",
+      value: "private, no-cache, no-store, max-age=0, must-revalidate",
+    },
+    {
+      key: "CDN-Cache-Control",
+      value: "private, no-store",
+    },
+    {
+      key: "Vary",
+      value: "RSC, Next-Router-State-Tree, Next-Router-Prefetch, Accept",
+    },
+  ];
+
+  return [
+    // ---------------------------------------------------------------
+    // REGRA 1: Protege quando tem a QUERY STRING (?_rsc=...)
+    // ---------------------------------------------------------------
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "query",
+          key: "_rsc",
+        },
+      ],
+      headers: RSCCacheHeaders,
+    },
+
+    // ---------------------------------------------------------------
+    // REGRA 2: Protege quando tem o HEADER 'RSC' (O lance do RSC: 1)
+    // (Essencial para redirects e requests onde a query string some)
+    // ---------------------------------------------------------------
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "header",
+          key: "RSC", // O Next.js verifica isso case-insensitive
+        },
+      ],
+      headers: RSCCacheHeaders,
+    },
+
+    // ---------------------------------------------------------------
+    // REGRA 3: Protege quando tem o HEADER de PREFETCH
+    // (Segurança extra para quando o link entra na tela)
+    // ---------------------------------------------------------------
+    {
+      source: "/:path*",
+      has: [
+        {
+          type: "header",
+          key: "Next-Router-Prefetch",
+        },
+      ],
+      headers: RSCCacheHeaders,
+    },
+  ];
+};
 
 const nextConfig: NextConfig = {
   turbopack: {},
@@ -81,6 +145,7 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      ...(RSCValidate() as Header[]),
       {
         source: "/api/((?!media).*)",
         headers: [{ key: "Cache-Control", value: "private, no-cache, no-store, must-revalidate" }],
